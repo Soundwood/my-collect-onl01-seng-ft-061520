9c0e999a5b16a4c18301f4bda83910774e0396df
Ben Hogan <j.benhogan@gmail.com>
2020-06-19 12:45:40 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
 
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+def my_collect(array) do |name|
+  name.split(" ").first
+end
 
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+def my_collect(collection) do |lang|
+  lang.upcase
+end
